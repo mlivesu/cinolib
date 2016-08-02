cf9415f02b33c847ab1287135d8bbb4268c66176
cino <cino.83@gmail.com>
2016-08-02 11:03:48 +0200

    working on 3D curves

---
+++ unstable/drawable_curve.cpp
@@ -0,0 +1,65 @@
+/****************************************************************************
+* Italian National Research Council                                         *
+* Institute for Applied Mathematics and Information Technologies, Genoa     *
+* IMATI-GE / CNR                                                            *
+*                                                                           *
+* Author: Marco Livesu (marco.livesu@gmail.com)                             *
+*                                                                           *
+* Copyright(C) 2016                                                         *
+* All rights reserved.                                                      *
+*                                                                           *
+* This file is part of CinoLib                                              *
+*                                                                           *
+* CinoLib is free software; you can redistribute it and/or modify           *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 3 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+****************************************************************************/
+#include <cinolib/unstable/drawable_curve.h>
+
+namespace cinolib
+{
+
+CINO_INLINE
+DrawableCurve::DrawableCurve() : Curve()
+{}
+
+CINO_INLINE
+DrawableCurve::DrawableCurve(const std::vector<vec3d> & samples) : Curve(samples)
+{}
+
+
+CINO_INLINE
+void DrawableCurve::draw() const
+{
+    double cylind_rad = bbox().diag()*0.01;
+
+    for(size_t i=1; i<samples().size(); ++i)
+    {
+        cylinder<vec3d>(samples()[i-1], samples()[i], cylind_rad, cylind_rad, RED);
+    }
+}
+
+
+CINO_INLINE
+vec3d DrawableCurve::scene_center() const
+{
+    return bbox().center();
+}
+
+
+CINO_INLINE
+float DrawableCurve::scene_radius() const
+{
+    return bbox().diag();
+}
+
+
+
+}
---
+++ unstable/drawable_curve.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+* Italian National Research Council                                         *
+* Institute for Applied Mathematics and Information Technologies, Genoa     *
+* IMATI-GE / CNR                                                            *
+*                                                                           *
+* Author: Marco Livesu (marco.livesu@gmail.com)                             *
+*                                                                           *
+* Copyright(C) 2016                                                         *
+* All rights reserved.                                                      *
+*                                                                           *
+* This file is part of CinoLib                                              *
+*                                                                           *
+* CinoLib is free software; you can redistribute it and/or modify           *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 3 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+****************************************************************************/
+#ifndef DRAWABLE_CURVE_H
+#define DRAWABLE_CURVE_H
+
+#include <cinolib/cinolib.h>
+#include <cinolib/drawable_object.h>
+#include <cinolib/unstable/curve.h>
+#include <cinolib/gl/draw_cylinder.h>
+#include <cinolib/colors.h>
+
+namespace cinolib
+{
+
+class DrawableCurve : public Curve, public DrawableObject
+{
+    public:
+
+        DrawableCurve();
+        DrawableCurve(const std::vector<vec3d> & samples);
+
+        void  draw()         const;
+        vec3d scene_center() const;
+        float scene_radius() const;
+};
+
+}
+
+#ifndef  CINO_STATIC_LIB
+#include "drawable_curve.cpp"
+#endif
+
+#endif // DRAWABLE_CURVE_H
