a7db396729c0e1d1038c08c85ab680f2d771321c
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-05-18 10:23:00 +0200

    added determinant for 2x2 matrices

---
+++ determinant.h
@@ -0,0 +1,53 @@
+/****************************************************************************
+* Italian National Research Council                                         *
+* Institute for Applied Mathematics and Information Technologies, Genoa     *
+* IMATI-GE / CNR                                                            *
+*                                                                           *
+* Author: Marco Livesu (marco.livesu@gmail.com)                             *
+*                                                                           *
+* Copyright(C) 2017                                                         *
+* All rights reserved.                                                      *
+*                                                                           *
+* This file is part of CinoLib                                              *
+*                                                                           *
+* CinoLib is free software; you can redistribute it and/or modify           *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 3 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+****************************************************************************/
+#ifndef CINO_DETERMINANT_H
+#define CINO_DETERMINANT_H
+
+#include <cinolib/cinolib.h>
+#include <cinolib/geometry/vec2.h>
+
+namespace cinolib
+{
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+double determinant_2x2(const double a00, const double a01, const double a10, const double a11)
+{
+    return ((a00*a11) - (a10*a01));
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+double determinant_2x2(const vec2d a0, const vec2d a1)
+{
+    return determinant_2x2(a0[0], a0[1], a1[0], a1[1]);
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+}
+
+#endif // CINO_DETERMINANT_H
