ea18fff82bc349be36d924c9413f8e16715c1676
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-06-02 11:57:30 +0200

    update license

--- meshes/trimesh/trimesh.cpp
+++ meshes/trimesh/trimesh.cpp
@@ -1,26 +1,33 @@
-/****************************************************************************
-* Italian National Research Council                                         *
-* Institute for Applied Mathematics and Information Technologies, Genoa     *
-* IMATI-GE / CNR                                                            *
-*                                                                           *
-* Author: Marco Livesu (marco.livesu@gmail.com)                             *
-*                                                                           *
-* Copyright(C) 2016                                                         *
-* All rights reserved.                                                      *
-*                                                                           *
-* This file is part of CinoLib                                              *
-*                                                                           *
-* CinoLib is free software; you can redistribute it and/or modify           *
-* it under the terms of the GNU General Public License as published by      *
-* the Free Software Foundation; either version 3 of the License, or         *
-* (at your option) any later version.                                       *
-*                                                                           *
-* This program is distributed in the hope that it will be useful,           *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
-* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
-* for more details.                                                         *
-****************************************************************************/
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
 #include <cinolib/meshes/trimesh/trimesh.h>
 #include <cinolib/bfs.h>
 #include <cinolib/timer.h>
