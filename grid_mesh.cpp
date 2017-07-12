58eef1458719d2ab710630c670343c94afa0f599
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-07-12 16:13:54 +0200

    added grid mesh generation utility

---
+++ grid_mesh.cpp
@@ -0,0 +1,57 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#include <cinolib/grid_mesh.h>
+
+namespace cinolib
+{
+
+CINO_INLINE
+Quadmesh<> grid_mesh(const uint quads_per_row, const uint quads_per_col)
+{
+    std::vector<vec3d> points;
+    std::vector<uint>  faces;
+    for(uint r=0; r<=quads_per_row; ++r)
+    for(uint c=0; c<=quads_per_col; ++c)
+    {
+        points.push_back(vec3d(r,c,0));
+
+        if (r<quads_per_row && c<quads_per_col)
+        {
+            faces.push_back(serialize_2D_index(r  , c,   quads_per_col+1));
+            faces.push_back(serialize_2D_index(r  , c+1, quads_per_col+1));
+            faces.push_back(serialize_2D_index(r+1, c+1, quads_per_col+1));
+            faces.push_back(serialize_2D_index(r+1, c,   quads_per_col+1));
+        }
+    }
+    return Quadmesh<>(points, faces);
+}
+
+}
