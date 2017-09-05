df86b35cc1178840c82b39d588906ffead8b245b
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-09-05 17:45:03 +0200

    add drawable segment soup

---
+++ include/cinolib/drawable_segment_soup.cpp
@@ -0,0 +1,78 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#include <cinolib/drawable_segment_soup.h>
+#include <cinolib/color.h>
+#include <cinolib/gl/draw_sphere.h>
+#include <cinolib/gl/draw_cylinder.h>
+
+namespace cinolib
+{
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+DrawableSegmentSoup::DrawableSegmentSoup()
+{
+    color     = Color::RED();
+    thickness = 1.0;
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+void DrawableSegmentSoup::draw(const float scene_size) const
+{
+    float cylind_rad = scene_size * 0.002 * thickness;
+
+    for(vec3d p : *this) sphere<vec3d>(p, cylind_rad, color.rgba);
+
+    for(uint i=0; i<size()/2; ++i)
+    {
+        cylinder<vec3d>(at(2*i+0), at(2*i+1), cylind_rad, cylind_rad, color.rgba);
+    }
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+void DrawableSegmentSoup::set_color(const Color & c)
+{
+    color = c;
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+void DrawableSegmentSoup::set_thickness(float t)
+{
+    thickness = t;
+}
+
+}
---
+++ include/cinolib/drawable_segment_soup.h
@@ -0,0 +1,88 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#ifndef CINO_DRAWABLE_SEGMENT_SOUP_H
+#define CINO_DRAWABLE_SEGMENT_SOUP_H
+
+#include <cinolib/cinolib.h>
+#include <cinolib/drawable_object.h>
+
+#include <vector>
+
+namespace cinolib
+{
+
+/* Utility class to render a soup of segments into a GL canvas.
+ * Segment endpoints are stored as a serialized list of vertices,
+ * that is:
+ *
+ * 1st segment: soup[0], soup[1]
+ * 2nd segment: soup[2], soup[3]
+ * ....
+ * nth segment : soup[2(n-1)], soup[2(n-1)+1]
+*/
+
+class DrawableSegmentSoup: public std::vector<vec3d>, public DrawableObject
+{
+    public:
+
+        explicit DrawableSegmentSoup();
+
+        //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        void  draw(const float scene_size=1) const;
+
+        //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        vec3d scene_center() const { return vec3d(); }
+        float scene_radius() const { return 0.0;     }
+
+        //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        ObjectType object_type()  const { return DRAWABLE_CURVE; }
+
+        //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        void set_color(const Color & c);
+        void set_thickness(float t);
+
+    private:
+
+        Color color;
+        float thickness;
+};
+
+}
+
+#ifndef  CINO_STATIC_LIB
+#include "drawable_segment_soup.cpp"
+#endif
+
+#endif // CINO_DRAWABLE_SEGMENT_SOUP_H
