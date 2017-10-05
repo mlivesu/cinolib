c7ec9175fbe8f6d4327732adcf0782c119377292
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-10-05 15:20:17 +0200

    add Shewchuk predicates

---
+++ include/cinolib/Shewchuk_predicates.h
@@ -0,0 +1,72 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#ifndef CINO_SHEWCHUK_PREDICATES
+#define CINO_SHEWCHUK_PREDICATES
+
+#include <cinolib/geometry/vec2.h>
+#include <cinolib/geometry/vec3.h>
+
+namespace cinolib
+{
+
+double orient2d    (double const * pa, double const * pb, double const * pc);
+double orient2d    (const  vec2d & pa, const  vec2d & pb, const  vec2d & pc);
+double orient2dfast(double const * pa, double const * pb, double const * pc);
+double orient2dfast(const  vec2d & pa, const  vec2d & pb, const  vec2d & pc);
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+double orient3d    (double const * pa, double const * pb, double const * pc, double const * pd);
+double orient3d    (const  vec3d & pa, const  vec3d & pb, const  vec3d & pc, const  vec3d & pd);
+double orient3dfast(double const * pa, double const * pb, double const * pc, double const * pd);
+double orient3dfast(const  vec3d & pa, const  vec3d & pb, const  vec3d & pc, const  vec3d & pd);
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+double incircle    (double const * pa, double const * pb, double const * pc, double const * pd);
+double incircle    (const  vec2d & pa, const  vec2d & pb, const  vec2d & pc, const  vec2d & pd);
+double incirclefast(double const * pa, double const * pb, double const * pc, double const * pd);
+double incirclefast(const  vec2d & pa, const  vec2d & pb, const  vec2d & pc, const  vec2d & pd);
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+double insphere    (double const * pa, double const * pb, double const * pc, double const * pd, double const * pe);
+double insphere    (const  vec3d & pa, const  vec3d & pb, const  vec3d & pc, const  vec3d & pd, const  vec3d & pe);
+double inspherefast(double const * pa, double const * pb, double const * pc, double const * pd, double const * pe);
+double inspherefast(const  vec3d & pa, const  vec3d & pb, const  vec3d & pc, const  vec3d & pd, const  vec3d & pe);
+
+}
+
+#ifndef  CINO_STATIC_LIB
+#include "Shewchuk_predicates.cpp"
+#endif
+
+#endif // CINO_SHEWCHUK_PREDICATES
