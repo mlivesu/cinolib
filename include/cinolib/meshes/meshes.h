ca1665bb23ec70175a07a19d36dcabc6fb31b10a
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-10-09 18:54:44 +0200

    meshes header includer

---
+++ include/cinolib/meshes/meshes.h
@@ -0,0 +1,50 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#ifndef CINO_MESHES_H
+#define CINO_MESHES_H
+
+// SURFACE MESHES
+#include <cinolib/meshes/trimesh.h>
+#include <cinolib/meshes/quadmesh.h>
+#include <cinolib/meshes/polygonmesh.h>
+#include <cinolib/meshes/drawable_trimesh.h>
+#include <cinolib/meshes/drawable_quadmesh.h>
+#include <cinolib/meshes/drawable_polygonmesh.h>
+
+// VOLUME MESHES
+#include <cinolib/meshes/tetmesh.h>
+#include <cinolib/meshes/hexmesh.h>
+#include <cinolib/meshes/polyhedralmesh.h>
+#include <cinolib/meshes/drawable_tetmesh.h>
+#include <cinolib/meshes/drawable_hexmesh.h>
+#include <cinolib/meshes/drawable_polyhedralmesh.h>
+
+#endif // CINO_MESHES_H
