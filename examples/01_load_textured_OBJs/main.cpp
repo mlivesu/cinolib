cff85194c0e5f80dfcde3c9d070f1074d1e17dd6
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-10-15 20:24:16 +0200

    update sample 01

--- examples/01_load_textured_OBJs/main.cpp
+++ examples/01_load_textured_OBJs/main.cpp
@@ -1,3 +1,15 @@
+/* This program reads a textured 3D model from an OBJ file and visualizes it
+ * in two separate GL canvases. On the first canvas the regular 3D shape is
+ * shown with a texture applied on it (texture seams are shown in red). On the
+ * second canvas the shape is shown flattened on the uv space.
+ *
+ * NOTE: if for some reason the texture is not automatically loaded, please
+ * click on the "Load Text2D" button on the control panel, and select the file
+ * "blub_texture.png". This should fix the problem.
+ *
+ * Enjoy!
+*/
+
 #include <QApplication>
 #include <cinolib/meshes/meshes.h>
 #include <cinolib/gui/qt/glcanvas.h>
@@ -31,16 +43,16 @@ int main(int argc, char **argv)
     m_uvw.copy_uvw_to_xyz(UVW_param);
     m_uvw.update_normals();
 
-    m_xyz.show_face_texture2D(TEXTURE_2D_BITMAP, 1.0, "../../data/blub_texture.png");
-    m_uvw.show_face_texture2D(TEXTURE_2D_BITMAP, 1.0, "../../data/blub_texture.png");
+    m_xyz.show_texture2D(TEXTURE_2D_BITMAP, 1.0, "../../data/blub_texture.png");
+    m_uvw.show_texture2D(TEXTURE_2D_BITMAP, 1.0, "../../data/blub_texture.png");
 
     gui_xyz.push_obj(&m_xyz);
     gui_uvw.push_obj(&m_uvw);
 
-    SurfaceMeshControlPanel<DrawableTrimesh<>> m_controls (&m_xyz, &gui_xyz);
-    SurfaceMeshControlPanel<DrawableTrimesh<>> m_controls2(&m_uvw, &gui_uvw);
-    m_controls.show();
-    m_controls2.show();
+    SurfaceMeshControlPanel<DrawableTrimesh<>> xyz_panel(&m_xyz, &gui_xyz);
+    SurfaceMeshControlPanel<DrawableTrimesh<>> uvw_panel(&m_uvw, &gui_uvw);
+    xyz_panel.show();
+    uvw_panel.show();
 
     return a.exec();
 }
