c12150c6bfff137ec8cadb1119003e673aaed200
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-10-16 15:15:27 +0200

    update examples

---
+++ examples/03_polyharmonic_functions_vol/03_polyharmonic_functions_vol.pro
@@ -0,0 +1,33 @@
+TEMPLATE        = app
+TARGET          = 03_polyharmonic_functions_vol
+QT             += core opengl xml
+CONFIG         += c++11
+CONFIG         -= app_bundle
+INCLUDEPATH    += /usr/local/include
+INCLUDEPATH    += /Users/cino/Documents/research/devel/lib/CinoLib/include
+DEPENDPATH     += /Users/cino/Documents/research/devel/lib/CinoLib/include # force recompilation if cinolib changes
+DEFINES        += CINOLIB_PROFILER_ENABLED
+
+
+# ------------------------------------ #
+# --------- WARNINGS CONTROL --------- #
+# ------------------------------------ #
+
+QMAKE_CXXFLAGS += -Wno-deprecated-declarations       # gluQuadric gluSphere and gluCylinde are deprecated in macOS 10.9
+
+# ------------------------------------ #
+# ------- EXTERNAL DEPENDENCIES ------ #
+# ------------------------------------ #
+
+# enable a OpenGL, QT and QGLViewer based GUI (used in cinolib/gui/qt/glcanvas.cpp)
+DEFINES      += CINOLIB_USES_OPENGL
+DEFINES      += CINOLIB_USES_QT_AND_QGLVIEWER
+INCLUDEPATH  += /Library/Frameworks/QGLViewer.framework/Versions/2/Headers
+QMAKE_LFLAGS *= -F/Library/Frameworks
+LIBS         += -framework QGLViewer
+
+# ------------------------------------ #
+# -------------- SOURCES ------------- #
+# ------------------------------------ #
+
+SOURCES += main.cpp
---
+++ examples/03_polyharmonic_functions_vol/main.cpp
@@ -0,0 +1,59 @@
+/* This sample program computes n-harmonic functions on a volume mesh.
+ * The harmonicity index is initially set to 1, and can be controlled
+ * via the spin box placed on top of the GL canvas.
+ *
+ * Enjoy!
+*/
+
+#include <QApplication>
+#include <QGridLayout>
+#include <QLabel>
+#include <QSpinBox>
+#include <cinolib/meshes/meshes.h>
+#include <cinolib/gui/qt/glcanvas.h>
+#include <cinolib/gui/qt/volume_mesh_control_panel.h>
+#include <cinolib/harmonic_map.h>
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+int main(int argc, char **argv)
+{
+    using namespace cinolib;
+
+    QApplication a(argc, argv);
+
+    QWidget window;
+    GLcanvas gui(&window);
+    QLabel label("Harmonicity index (1 => harmonic, 2 => bi-harmonic, ...):", &window);
+    QSpinBox n_harmonicity(&window);
+    n_harmonicity.setMaximum(10);
+    n_harmonicity.setMinimum(1);
+    QGridLayout layout;
+    layout.addWidget(&label,0,0);
+    layout.addWidget(&n_harmonicity,0,1);
+    layout.addWidget(&gui,1,0,1,2);
+    window.setLayout(&layout);
+    window.show();
+
+    DrawableTetmesh<> m("../../data/sphere.MESH");
+    m.show_out_texture1D(TEXTURE_1D_HSV_RAMP_W_ISOLINES);
+    m.show_in_texture1D (TEXTURE_1D_HSV_RAMP_W_ISOLINES);
+    gui.push_obj(&m);
+
+    VolumeMeshControlPanel<DrawableTetmesh<>> m_controls(&m, &gui);
+    m_controls.show();
+
+    // use the spin box to increase the harmonicity index
+    QSpinBox::connect(&n_harmonicity, static_cast<void(QSpinBox::*)(int)>(&QSpinBox::valueChanged), [&]()
+    {
+
+        std::map<uint,double> bc = {{0,0.0}, {999,1.0}}; // Dirichlet boundary conditions
+        harmonic_map(m, bc, n_harmonicity.value(), COTANGENT, SIMPLICIAL_LDLT).copy_to_mesh(m);
+        m.updateGL();
+        gui.updateGL();
+    });
+
+    n_harmonicity.valueChanged(1); // start with a 1-harmonic map
+
+    return a.exec();
+}
