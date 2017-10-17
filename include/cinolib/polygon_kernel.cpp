6691bf90fcb1383637f35d4beca5a47c282b900d
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-10-17 10:57:24 +0200

    polygon kernel

---
+++ include/cinolib/polygon_kernel.cpp
@@ -0,0 +1,126 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#include <cinolib/polygon_kernel.h>
+
+#ifdef CINOLIB_USES_BOOST
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry.hpp>
+//
+typedef boost::geometry::model::d2::point_xy<double> BoostPoint;
+typedef boost::geometry::model::polygon<BoostPoint>  BoostPolygon;
+#endif
+
+namespace cinolib
+{
+
+#ifdef CINOLIB_USES_BOOST
+
+CINO_INLINE
+double polygon_kernel(const std::vector<vec2d> & poly,
+                            std::vector<vec2d> & kernel)
+{
+    kernel.clear();
+    if (poly.empty()) return 0;
+
+    // https://stackoverflow.com/questions/20016600/negative-infinity
+    static_assert(std::numeric_limits<double>::is_iec559, "IEEE 754 required");
+    double inf = std::numeric_limits<double>::infinity();
+
+    // define 2d axis aligned bbox
+    vec2d min( inf,  inf);
+    vec2d max(-inf, -inf);
+    for(const vec2d & p : poly)
+    {
+        min = min.min(p);
+        max = max.max(p);
+    }
+    double delta = min.dist(max);
+
+    // define half spaces
+    std::vector<BoostPolygon> half_spaces;
+    for(uint i=0; i<poly.size(); ++i)
+    {
+        vec2d A   = poly.at(i);
+        vec2d B   = poly.at((i+1)%poly.size());
+        vec2d u = B - A; u.normalize(); // edge direction
+        vec2d v = vec2d(-u.y(), u.x()); // direction orthogonal to u (rotated CCW)
+        A -= u * delta;
+        B += u * delta;
+        vec2d C = B + v * delta;
+        vec2d D = A + v * delta;
+
+        BoostPolygon p;
+        boost::geometry::append(p, BoostPoint(A.x(), A.y()));
+        boost::geometry::append(p, BoostPoint(B.x(), B.y()));
+        boost::geometry::append(p, BoostPoint(C.x(), C.y()));
+        boost::geometry::append(p, BoostPoint(D.x(), D.y()));
+        boost::geometry::correct(p);
+        half_spaces.push_back(p);
+    }
+
+    // define kernel as intersection of half-spaces
+    BoostPolygon kernel_boost = half_spaces.front();
+    for(uint i=1; i<half_spaces.size(); ++i)
+    {
+        std::vector<BoostPolygon> res;
+        boost::geometry::intersection(kernel_boost, half_spaces.at(i), res);
+        if (res.empty()) return 0; // no kernel
+        assert(res.size()==1);     // not sure the case "size > 1" can never happen...
+        kernel_boost = res.front();
+    }
+
+    // convert from BoostPolygon to vec2d array
+    for(uint i=0; i<kernel_boost.outer().size()-1; ++i)
+    {
+        kernel.push_back(vec2d(boost::geometry::get<0>(kernel_boost.outer()[i]),
+                               boost::geometry::get<1>(kernel_boost.outer()[i])));
+    }
+
+    // Boost uses CW ordered vertices, so here I make the kernel CCW
+    std::reverse(kernel.begin(), kernel.end());
+
+    return boost::geometry::area(kernel_boost);
+}
+
+#else
+
+CINO_INLINE
+double polygon_kernel(const std::vector<vec2d> &,
+                            std::vector<vec2d> &)
+{
+    std::cerr << "ERROR : Boost Polygon disabled. Recompile defining symbol CINOLIB_USES_BOOST" << endl;
+    exit(-1);
+}
+
+#endif
+
+}
---
+++ include/cinolib/polygon_kernel.h
@@ -0,0 +1,64 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#ifndef CINO_POLYGON_KERNEL_H
+#define CINO_POLYGON_KERNEL_H
+
+#include <cinolib/cinolib.h>
+#include <cinolib/geometry/vec2.h>
+
+namespace cinolib
+{
+
+/* Computes the kernel of a 2D polygon (the locus of points which are
+ * visible from any corner). The procedure is based on the Boost Polygon
+ * Library.
+ *
+ * For each oriented edge in the polygon we generate a big enough quad
+ * representing the positive half-space, and we then intersect all the
+ * positive half-spaces together using boolean operations.
+ *
+ * Polygon representation: a polygon is a vector of points ordered CCW to
+ * form a closed loop (first and last points are adjacent).
+ *
+ * The function returns the area of the kernel (i.e. 0 means the polygon has no kernel)
+*/
+
+CINO_INLINE
+double polygon_kernel(const std::vector<vec2d> & poly,
+                            std::vector<vec2d> & kernel);
+
+}
+
+#ifndef  CINO_STATIC_LIB
+#include "polygon_kernel.cpp"
+#endif
+
+#endif // CINO_POLYGON_KERNEL_H
