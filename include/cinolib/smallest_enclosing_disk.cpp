164fb61dd684fadc51361e4e7cf314ae1753674c
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2018-01-07 19:13:05 +0100

    recursive implementation of Welzl's algorithm for smallest disk computation in 2D

---
+++ include/cinolib/smallest_enclosing_disk.cpp
@@ -0,0 +1,153 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#include <cinolib/smallest_enclosing_disk.h>
+
+#include <numeric>
+#include <algorithm>
+#include <random>
+#include <chrono>
+
+namespace cinolib
+{
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+std::pair<vec2d,double> make_disk(const std::vector<vec2d> & points,
+                                  const uint                 id)
+{
+    std::pair<vec2d,double> disk;
+    disk.first  = points.at(id);
+    disk.second = 0.0;
+    return disk;
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+std::pair<vec2d,double> make_disk(const std::vector<vec2d> & points,
+                                    const uint               id0,
+                                    const uint               id1)
+{
+    std::pair<vec2d,double> disk;
+    disk.first = (points.at(id0) + points.at(id1))*0.5;
+    disk.second = disk.first.dist(points.at(id1));
+    return disk;
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+std::pair<vec2d,double> make_disk(const std::vector<vec2d> & points,
+                                    const uint               id0,
+                                    const uint               id1,
+                                    const uint               id2)
+{
+    std::pair<vec2d,double> disk;
+    double xd, yd, side_a, side_b, side_c, s;
+    double term1, term2, term3, denom;
+    xd = points.at(id0).x()-points.at(id1).x();
+    yd = points.at(id0).y()-points.at(id1).y();
+    side_a = sqrt(xd*xd + yd*yd);
+    xd = points.at(id1).x()-points.at(id2).x();
+    yd = points.at(id1).y()-points.at(id2).y();
+    side_b = sqrt(xd*xd + yd*yd);
+    xd = points.at(id2).x()-points.at(id0).x();
+    yd = points.at(id2).y()-points.at(id0).y();
+    side_c = sqrt(xd*xd + yd*yd);
+    s = (side_a + side_b + side_c)*0.5;
+    denom = sqrt(s*(s - side_a)*(s - side_b)*(s - side_c));
+    disk.second = side_a * side_b * side_c * 0.25 / denom;
+    term1 = points.at(id0).x()*points.at(id0).x() + points.at(id0).y()*points.at(id0).y();
+    term2 = points.at(id1).x()*points.at(id1).x() + points.at(id1).y()*points.at(id1).y();
+    term3 = points.at(id2).x()*points.at(id2).x() + points.at(id2).y()*points.at(id2).y();
+    denom = 2.0*(points.at(id0).x()*(points.at(id1).y()-points.at(id2).y()) +
+        points.at(id1).x()*(points.at(id2).y()-points.at(id0).y()) + points.at(id2).x()*(points.at(id0).y()-points.at(id1).y()));
+    disk.first.x() = (term1*(points.at(id1).y()-points.at(id2).y()) + term2*(points.at(id2).y()-points.at(id0).y()) +
+        term3*(points.at(id0).y()-points.at(id1).y()))/denom;
+    disk.first.y() = (term1*(points.at(id2).x()-points.at(id1).x()) + term2*(points.at(id0).x()-points.at(id2).x()) +
+        term3*(points.at(id1).x()-points.at(id0).x()))/denom;
+    return disk;
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// Smallest Enclosing Disks (Balls and Ellipsoids)
+// Emo Welzl
+// New Results and Trends in Computer Science
+// Lecture Notes in Computer Science, 555 (1991), pp359-370
+//
+CINO_INLINE
+std::pair<vec2d,double> Welzl_algorithm(const std::vector<vec2d> & points,
+                                        const std::vector<uint>  & P,
+                                        const uint                 nP,
+                                              std::vector<uint>  & R,
+                                        const uint                 nR)
+{
+    if(nP == 1)
+    {
+        if(nR == 0) return make_disk(points, P.at(0));
+        if(nR == 1) return make_disk(points, P.at(0), R.at(0));
+    }
+    else if(nP == 0)
+    {
+        if(nR == 1) return make_disk(points, R.at(0));
+        if(nR == 2) return make_disk(points, R.at(0), R.at(1));
+    }
+    if (nR == 3) return make_disk(points, R.at(0), R.at(1), R.at(2));
+
+    std::pair<vec2d,double> c = Welzl_algorithm(points, P, nP-1, R, nR);
+    if (c.first.dist(points.at(P.at(nP-1))) < c.second) return c;
+    R.at(nR) = P.at(nP-1);
+    return Welzl_algorithm(points, P, nP-1, R, nR+1);
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+CINO_INLINE
+void smallest_enclosing_disk(const std::vector<vec2d> & points,
+                                   vec2d              & center,
+                                   double             & radius)
+{
+    std::vector<uint> P(points.size());
+    std::vector<uint> R(points.size(),0);
+    std::iota(P.begin(), P.end(), 0);
+
+    // randomize points (http://www.cplusplus.com/reference/algorithm/shuffle/)
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::shuffle(P.begin(), P.end(), std::default_random_engine(seed));
+
+    auto min_disk = Welzl_algorithm(points, P, P.size(), R, 0);
+    center = min_disk.first;
+    radius = min_disk.second;
+}
+
+}
---
+++ include/cinolib/smallest_enclosing_disk.h
@@ -0,0 +1,61 @@
+/*********************************************************************************
+*  Copyright(C) 2016: Marco Livesu                                               *
+*  All rights reserved.                                                          *
+*                                                                                *
+*  This file is part of CinoLib                                                  *
+*                                                                                *
+*  CinoLib is dual-licensed:                                                     *
+*                                                                                *
+*   - For non-commercial use you can redistribute it and/or modify it under the  *
+*     terms of the GNU General Public License as published by the Free Software  *
+*     Foundation; either version 3 of the License, or (at your option) any later *
+*     version.                                                                   *
+*                                                                                *
+*   - If you wish to use it as part of a commercial software, a proper agreement *
+*     with the Author(s) must be reached, based on a proper licensing contract.  *
+*                                                                                *
+*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
+*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
+*                                                                                *
+*  Author(s):                                                                    *
+*                                                                                *
+*     Marco Livesu (marco.livesu@gmail.com)                                      *
+*     http://pers.ge.imati.cnr.it/livesu/                                        *
+*                                                                                *
+*     Italian National Research Council (CNR)                                    *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
+*     Via de Marini, 6                                                           *
+*     16149 Genoa,                                                               *
+*     Italy                                                                      *
+**********************************************************************************/
+#ifndef CINO_SMALLEST_ENCLOSING_DISK_H
+#define CINO_SMALLEST_ENCLOSING_DISK_H
+
+#include <cinolib/cinolib.h>
+#include <cinolib/geometry/vec2.h>
+#include <vector>
+
+namespace cinolib
+{
+
+// Solve the smallest-circle problem (or minimum covering circle problem)
+// using a recirsive implementation of the Welzl's algorithm. References:
+//
+//    1) https://en.wikipedia.org/wiki/Smallest-circle_problem
+//
+//    2) Smallest Enclosing Disks (Balls and Ellipsoids)
+//       Emo Welzl
+//       New Results and Trends in Computer Science
+//       Lecture Notes in Computer Science, 555 (1991), pp359-370
+//
+CINO_INLINE
+void smallest_enclosing_disk(const std::vector<vec2d> & points,
+                                   vec2d              & center,
+                                   double             & radius);
+}
+
+#ifndef  CINO_STATIC_LIB
+#include "smallest_enclosing_disk.cpp"
+#endif
+
+#endif // CINO_SMALLEST_ENCLOSING_DISK_H
