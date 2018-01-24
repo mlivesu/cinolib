747a1956e35104ac1b49f228482b7f44debf754f
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2018-01-24 11:48:28 +0100

    bugfix

--- examples/07_textured_OBJs/main.cpp
+++ examples/07_textured_OBJs/main.cpp
@@ -13,7 +13,6 @@
 #include <QApplication>
 #include <cinolib/meshes/meshes.h>
 #include <cinolib/gui/qt/glcanvas.h>
-#include <cinolib/gui/qt/surface_mesh_control_panel.h>
 
 using namespace cinolib;
 
@@ -35,26 +34,20 @@ int main(int argc, char **argv)
     read_OBJ(s.c_str(), xyz, uvw, poly);
 
     DrawableTrimesh<> m_xyz(xyz, poly);
-
-    for(uint vid=0; vid<m_xyz.num_verts(); ++vid) m_xyz.vert_data(vid).uvw    = uvw.at(vid);
-    for(uint eid=0; eid<m_xyz.num_edges(); ++eid) m_xyz.edge_data(eid).marked = m_xyz.edge_is_boundary(eid);
-    m_xyz.updateGL();
+    for(uint vid=0; vid<m_xyz.num_verts(); ++vid) m_xyz.vert_data(vid).uvw = uvw.at(vid);
+    m_xyz.edge_mark_boundaries();
 
     DrawableTrimesh<> m_uvw = m_xyz;
     m_uvw.copy_uvw_to_xyz(UVW_param);
     m_uvw.update_normals();
 
     s = (argc==3) ? std::string(argv[2]) : std::string(DATA_PATH) + "/blub_texture.png";
+
     m_xyz.show_texture2D(TEXTURE_2D_BITMAP, 1.0, s.c_str());
-    m_uvw.show_texture2D(TEXTURE_2D_BITMAP, 1.0, s.c_str());
-
     gui_xyz.push_obj(&m_xyz);
-    gui_uvw.push_obj(&m_uvw);
 
-    SurfaceMeshControlPanel<DrawableTrimesh<>> xyz_panel(&m_xyz, &gui_xyz);
-    SurfaceMeshControlPanel<DrawableTrimesh<>> uvw_panel(&m_uvw, &gui_uvw);
-    xyz_panel.show();
-    uvw_panel.show();
+    m_uvw.show_texture2D(TEXTURE_2D_BITMAP, 1.0, s.c_str());
+    gui_uvw.push_obj(&m_uvw);
 
     return a.exec();
 }
