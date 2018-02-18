0b70e793f6a426c795dd0af0f1cc62257ebb81cb
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2018-02-18 20:25:34 +0100

    added vert picking to example on surface harmonic maps

--- examples/09_polyharmonic_functions_srf/main.cpp
+++ examples/09_polyharmonic_functions_srf/main.cpp
@@ -1,18 +1,35 @@
 /* This sample program computes n-harmonic functions on a surface mesh.
+ *
  * The harmonicity index is initially set to 1, and can be controlled
  * via the spin box placed on top of the GL canvas.
  *
+ * Maxima and minima can be arbitrarily placed on the surface by vertex
+ * picking. Use CMD + click to add a maximum, and SHIFT + click to add
+ * a minimum.
+ *
  * Enjoy!
 */
-
 #include <QApplication>
 #include <QGridLayout>
+#include <QPushButton>
 #include <QLabel>
 #include <QSpinBox>
+#include <algorithm>
 #include <cinolib/meshes/meshes.h>
-#include <cinolib/gui/qt/glcanvas.h>
 #include <cinolib/harmonic_map.h>
 #include <cinolib/profiler.h>
+#include <cinolib/gui/qt/glcanvas.h>
+#include <cinolib/gui/qt/surface_mesh_control_panel.h>
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+uint closest_vertex(const cinolib::vec3d & p, const cinolib::Trimesh<> & m)
+{
+    std::vector<std::pair<double,uint>> closest;
+    for(uint vid=0; vid<m.num_verts(); ++vid) closest.push_back(std::make_pair(m.vert(vid).dist(p),vid));
+    std::sort(closest.begin(), closest.end());
+    return closest.front().second;
+}
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -23,37 +40,95 @@ int main(int argc, char **argv)
     QApplication a(argc, argv);
 
     QWidget window;
-    GLcanvas gui(&window);    
-    QLabel label("Harmonicity index (1 => harmonic, 2 => bi-harmonic, ...):", &window);
+    GLcanvas gui(&window);
+    QPushButton but_harmonic("Harmonic Field",&window);
+    QPushButton but_reset("Reset",&window);
     QSpinBox n_harmonicity(&window);
     n_harmonicity.setMaximum(5);
     n_harmonicity.setMinimum(1);
     QGridLayout layout;
-    layout.addWidget(&label,0,0);
+    layout.addWidget(new QLabel("Harmonicity (harmonic, bi-harmonic, ...):"),0,0);
     layout.addWidget(&n_harmonicity,0,1);
-    layout.addWidget(&gui,1,0,1,2);
+    layout.addWidget(&but_harmonic,0,2);
+    layout.addWidget(&but_reset,0,9);
+    layout.addWidget(&gui,2,0,1,10);
     window.setLayout(&layout);
     window.show();
-    window.resize(800,600);
+    window.resize(600,600);
+    gui.push_marker(vec2i(10, gui.height()-20), "CMD + click to add a maximum");
+    gui.push_marker(vec2i(10, gui.height()-40), "SHIFT + click to add a minimum");
 
-    std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/circle.obj";
+    std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/bunny.obj";
     DrawableTrimesh<> m(s.c_str());
-    m.show_texture1D(TEXTURE_1D_HSV_RAMP_W_ISOLINES);
     gui.push_obj(&m);
 
-    // use the spin box to increase the harmonicity index
     Profiler profiler;
-    QSpinBox::connect(&n_harmonicity, static_cast<void(QSpinBox::*)(int)>(&QSpinBox::valueChanged), [&]()
+    std::map<uint,double> dirichlet_bcs;
+    bool has_at_least_one_min = false;
+    bool has_at_least_one_max = false;
+
+    gui.callback_mouse_press = [&](GLcanvas *c, QMouseEvent *e)
     {
-        std::map<uint,double> bc = {{7,0.0}, {99,1.0}}; // Dirichlet boundary conditions
-        profiler.push("harmonic_map");
-        harmonic_map(m, bc, n_harmonicity.value(), COTANGENT).copy_to_mesh(m);
-        profiler.pop();
-        m.updateGL();
+        if (e->modifiers() == Qt::ControlModifier)  // add maximum
+        {
+            vec3d p;
+            vec2i click(e->x(), e->y());
+            if (c->unproject(click, p))
+            {
+                uint vid = closest_vertex(p,m);
+                dirichlet_bcs[vid] = 1.0;
+                has_at_least_one_max = true;
+                c->push_marker(m.vert(vid), "", Color::BLUE());
+                c->updateGL();
+            }
+        }
+        else if (e->modifiers() == Qt::ShiftModifier) // add minimum
+        {
+            vec3d p;
+            vec2i click(e->x(), e->y());
+            if (c->unproject(click, p))
+            {
+                uint vid = closest_vertex(p,m);
+                dirichlet_bcs[vid] = 0.0;
+                has_at_least_one_min = true;
+                c->push_marker(m.vert(vid), "", Color::RED());
+                c->updateGL();
+            }
+        }
+        return;
+    };
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    QPushButton::connect(&but_harmonic, &QPushButton::clicked, [&]()
+    {
+        if (!dirichlet_bcs.empty() && has_at_least_one_min && has_at_least_one_max)
+        {
+            profiler.push("harmonic_map");
+            harmonic_map(m, dirichlet_bcs, n_harmonicity.value(), COTANGENT).copy_to_mesh(m);
+            profiler.pop();
+            m.show_texture1D(TEXTURE_1D_ISOLINES);
+            gui.updateGL();
+        }
+    });
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    QPushButton::connect(&but_reset, &QPushButton::clicked, [&]()
+    {
+        has_at_least_one_min = false;
+        has_at_least_one_max = false;
+        dirichlet_bcs.clear();
+        m.show_poly_color();
+        gui.pop_all_markers();
+        gui.push_marker(vec2i(10, gui.height()-20), "CMD + click to add a maximum");
+        gui.push_marker(vec2i(10, gui.height()-40), "SHIFT + click to add a minimum");
         gui.updateGL();
     });
 
-    n_harmonicity.valueChanged(1); // start with a 1-harmonic map
+    // CMD+1 to show mesh controls.
+    SurfaceMeshControlPanel<DrawableTrimesh<>> panel(&m, &gui);
+    QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_1), &gui), &QShortcut::activated, [&](){panel.show();});
 
     return a.exec();
 }
