12297f361eecb5039c85bc01f8d5f4c3759b7a3c
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2018-07-04 00:06:12 +0200

    naming bugfix

--- include/cinolib/bfs.h
+++ include/cinolib/bfs.h
@@ -94,7 +94,7 @@ void bfs_on_dual_w_edge_barriers(const AbstractPolygonMesh<M,V,E,P> & m,
 //
 template<class M, class V, class E, class F, class P>
 CINO_INLINE
-void bfs_on_dual_w_edge_barriers(const AbstractPolyhedralMesh<M,V,E,F,P> & m,
+void bfs_on_dual_w_face_barriers(const AbstractPolyhedralMesh<M,V,E,F,P> & m,
                                  const uint                                source,
                                  const std::vector<bool>                 & mask_faces, // if mask[f] = true, bfs canno expand through face f
                                  std::unordered_set<uint>                & visited);
