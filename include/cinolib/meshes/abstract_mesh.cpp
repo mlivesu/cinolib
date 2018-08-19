8481e872bda830726d821a90ed4eef0fcbe85772
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2018-08-19 09:45:22 +0200

    bugfix

--- include/cinolib/meshes/abstract_mesh.cpp
+++ include/cinolib/meshes/abstract_mesh.cpp
@@ -318,12 +318,12 @@ template<class M, class V, class E, class P>
 CINO_INLINE
 std::vector<uint> AbstractMesh<M,V,E,P>::adj_e2e(const uint eid) const
 {
-    // WARNING: if two verts are connected by a second edge (!= eid), such edge will be duplicated
-    std::vector<uint> e_list;
+    std::set<uint> unique_e_list;
     uint v0 = this->edge_vert_id(eid,0);
     uint v1 = this->edge_vert_id(eid,1);
-    for(uint nbr : this->adj_v2e(v0)) if(nbr != eid) e_list.push_back(nbr);
-    for(uint nbr : this->adj_v2e(v1)) if(nbr != eid) e_list.push_back(nbr);
+    for(uint nbr : this->adj_v2e(v0)) if(nbr != eid) unique_e_list.insert(nbr);
+    for(uint nbr : this->adj_v2e(v1)) if(nbr != eid) unique_e_list.insert(nbr);
+    std::vector<uint> e_list(unique_e_list.begin(), unique_e_list.end());
     return e_list;
 }
 
