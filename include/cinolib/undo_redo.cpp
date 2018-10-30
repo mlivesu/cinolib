49b3042ace72c23bce9a0deb2782957616510f6c
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2018-10-30 10:20:17 +0100

    moved to MIT license

--- include/cinolib/undo_redo.cpp
+++ include/cinolib/undo_redo.cpp
@@ -1,33 +1,38 @@
-/*********************************************************************************
-*  Copyright(C) 2016: Marco Livesu                                               *
-*  All rights reserved.                                                          *
-*                                                                                *
-*  This file is part of CinoLib                                                  *
-*                                                                                *
-*  CinoLib is dual-licensed:                                                     *
-*                                                                                *
-*   - For non-commercial use you can redistribute it and/or modify it under the  *
-*     terms of the GNU General Public License as published by the Free Software  *
-*     Foundation; either version 3 of the License, or (at your option) any later *
-*     version.                                                                   *
-*                                                                                *
-*   - If you wish to use it as part of a commercial software, a proper agreement *
-*     with the Author(s) must be reached, based on a proper licensing contract.  *
-*                                                                                *
-*  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE       *
-*  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.     *
-*                                                                                *
-*  Author(s):                                                                    *
-*                                                                                *
-*     Marco Livesu (marco.livesu@gmail.com)                                      *
-*     http://pers.ge.imati.cnr.it/livesu/                                        *
-*                                                                                *
-*     Italian National Research Council (CNR)                                    *
-*     Institute for Applied Mathematics and Information Technologies (IMATI)     *
-*     Via de Marini, 6                                                           *
-*     16149 Genoa,                                                               *
-*     Italy                                                                      *
-**********************************************************************************/
+/********************************************************************************
+*  This file is part of CinoLib                                                 *
+*  Copyright(C) 2016: Marco Livesu                                              *
+*                                                                               *
+*  The MIT License                                                              *
+*                                                                               *
+*  Permission is hereby granted, free of charge, to any person obtaining a      *
+*  copy of this software and associated documentation files (the "Software"),   *
+*  to deal in the Software without restriction, including without limitation    *
+*  the rights to use, copy, modify, merge, publish, distribute, sublicense,     *
+*  and/or sell copies of the Software, and to permit persons to whom the        *
+*  Software is furnished to do so, subject to the following conditions:         *
+*                                                                               *
+*  The above copyright notice and this permission notice shall be included in   *
+*  all copies or substantial portions of the Software.                          *
+*                                                                               *
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
+*  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE *
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      *
+*  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS *
+*  IN THE SOFTWARE.                                                             *
+*                                                                               *
+*  Author(s):                                                                   *
+*                                                                               *
+*     Marco Livesu (marco.livesu@gmail.com)                                     *
+*     http://pers.ge.imati.cnr.it/livesu/                                       *
+*                                                                               *
+*     Italian National Research Council (CNR)                                   *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)    *
+*     Via de Marini, 6                                                          *
+*     16149 Genoa,                                                              *
+*     Italy                                                                     *
+*********************************************************************************/
 #include <cinolib/undo_redo.h>
 #include <cinolib/cino_inline.h>
 #include <assert.h>
