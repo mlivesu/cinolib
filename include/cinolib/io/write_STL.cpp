85d77b3d634e605c7a23bf0d5773f21012180adc
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2019-01-31 18:43:12 +0100

    Added possibility to save trimesh using STL format

---
+++ include/cinolib/io/write_STL.cpp
@@ -0,0 +1,79 @@
+/********************************************************************************
+*  This file is part of CinoLib                                                 *
+*  Copyright(C) 2016: Marco Livesu                                              *
+*                                                                               *
+*  The MIT License                                                              *
+*                                                                               *
+*  Permission is hereby granted, free of charge, to any person obtaining a      *
+*  copy of this software and associated documentation files (the "Software"),   *
+*  to deal in the Software without restriction, including without limitation    *
+*  the rights to use, copy, modify, merge, publish, distribute, sublicense,     *
+*  and/or sell copies of the Software, and to permit persons to whom the        *
+*  Software is furnished to do so, subject to the following conditions:         *
+*                                                                               *
+*  The above copyright notice and this permission notice shall be included in   *
+*  all copies or substantial portions of the Software.                          *
+*                                                                               *
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
+*  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE *
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      *
+*  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS *
+*  IN THE SOFTWARE.                                                             *
+*                                                                               *
+*  Author(s):                                                                   *
+*                                                                               *
+*     Marco Livesu (marco.livesu@gmail.com)                                     *
+*     http://pers.ge.imati.cnr.it/livesu/                                       *
+*                                                                               *
+*     Italian National Research Council (CNR)                                   *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)    *
+*     Via de Marini, 6                                                          *
+*     16149 Genoa,                                                              *
+*     Italy                                                                     *
+*********************************************************************************/
+#include <cinolib/io/write_STL.h>
+
+
+#include <iostream>
+
+namespace cinolib
+{
+
+CINO_INLINE
+void write_STL(const char                               * filename,
+               const std::vector<double>                 & xyz,
+               const std::vector<std::vector<uint>>     & tri,
+               const std::vector<double>                & tri_normals)
+{
+    setlocale(LC_NUMERIC, "en_US.UTF-8"); // makes sure "." is the decimal separator
+
+    std::cout << "Saving " << filename << std::endl;
+
+    FILE *fp = fopen(filename, "w");
+
+    if(!fp)
+    {
+        std::cerr << "ERROR : " << __FILE__ << ", line " << __LINE__ << " : save_STL() : couldn't save file " << filename << std::endl;
+        exit(-1);
+    }
+
+    fprintf(fp, "solid T_MESH\n");
+
+    for (uint pid=0; pid < tri.size(); pid++)
+    {
+        fprintf(fp, " facet normal %f %f %f\n", tri_normals.at(pid*3+0), tri_normals.at(pid*3+1), tri_normals.at(pid*3+2));
+        fprintf(fp, "  outer loop\n");
+        fprintf(fp, "   vertex %f %f %f\n", xyz.at(tri.at(pid).at(0)*3+0), xyz.at(tri.at(pid).at(0)*3+1), xyz.at(tri.at(pid).at(0)*3+2));
+        fprintf(fp, "   vertex %f %f %f\n", xyz.at(tri.at(pid).at(1)*3+0), xyz.at(tri.at(pid).at(1)*3+1), xyz.at(tri.at(pid).at(1)*3+2));
+        fprintf(fp, "   vertex %f %f %f\n", xyz.at(tri.at(pid).at(2)*3+0), xyz.at(tri.at(pid).at(2)*3+1), xyz.at(tri.at(pid).at(2)*3+2));
+        fprintf(fp, "  endloop\n");
+        fprintf(fp, " endfacet\n");
+    }
+    fprintf(fp, "endsolid T_MESH\n");
+
+    fclose(fp);
+}
+
+}
