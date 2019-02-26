585443bdbcf825f5089ed7dd478a6a6567c68078
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2019-02-26 11:10:17 -0500

    bugfix

--- examples/28_tetrahedralize/main.cpp
+++ examples/28_tetrahedralize/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
     }
 
     Polygonmesh<> m(argv[1]);
-    std::string flags = (argc<2) ? "" : argv[2];
+    std::string flags = (argc<3) ? "" : argv[2];
     std::vector<vec3d> v_out;
     std::vector<uint>  e_in, p_out;
     tetgen_wrap(m.vector_verts(), m.vector_polys(), e_in, flags.c_str(), v_out, p_out);
