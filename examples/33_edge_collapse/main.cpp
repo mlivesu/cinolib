08b30022b35e6e6cb30516dd5d95c92c88efeb18
Marco Livesu <marco.livesu@ge.imati.cnr.it>
2019-05-27 08:29:55 +0200

    Update main.cpp

--- examples/33_edge_collapse/main.cpp
+++ examples/33_edge_collapse/main.cpp
@@ -44,7 +44,6 @@ int main(int argc, char **argv)
                     {
                         converged = false;
                         m.updateGL();
-                        m.edge_mark_boundaries();
                         c->updateGL_strict();
                     }
                 }
