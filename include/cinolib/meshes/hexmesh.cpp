b392ea4ba084736947dfb849ef309379788c4bd7
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2019-10-29 16:39:18 +0100

    bugfix on mesh exporter

--- include/cinolib/meshes/hexmesh.cpp
+++ include/cinolib/meshes/hexmesh.cpp
@@ -138,8 +138,8 @@ void Hexmesh<M,V,E,F,P>::save(const char * filename) const
     std::string str(filename);
     std::string filetype = "." + get_file_extension(str);
 
-    if (filetype.compare("mesh") == 0 ||
-        filetype.compare("MESH") == 0)
+    if (filetype.compare(".mesh") == 0 ||
+        filetype.compare(".MESH") == 0)
     {
         if(this->polys_are_labeled())
         {
