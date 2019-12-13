1953a7bc054530c4cf6323bce19a5b77326ed171
gianmarcher <gianmarcher@gmail.com>
2019-12-13 10:42:22 +0100

    save_mesh fixed octree contains fixed

--- include/cinolib/octree.cpp
+++ include/cinolib/octree.cpp
@@ -367,7 +367,7 @@ bool Octree::contains(const vec3d & p, uint & id, const double eps) const
             for(int i=0; i<8; ++i)
             {
                 if(print_debug_info) ++aabb_queries;
-                if(node->children[i]->bbox.contains(p,true)) lifo.push(node->children[i]);
+                if(node->children[i]->bbox.contains(p,false)) lifo.push(node->children[i]);
             }
         }
         else
