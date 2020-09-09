0e2654e47b551b4ef4d05c1ec50773696e57e52e
P.E.Mazniker <60963077+goodengineer@users.noreply.github.com>
2020-09-09 16:53:16 +0300

    Update main.cpp

--- examples/04_base_app_tetmesh/main.cpp
+++ examples/04_base_app_tetmesh/main.cpp
@@ -1,7 +1,6 @@
 /* This is a base application for cinolib (https://github.com/maxicino/cinolib).
  * It will show a GL canvas (and associated control panel) to interact
  * with a tetrahedral mesh.
- *
  * In case you don't need a GUI, you can drop the "Drawable" prefix from the mesh data type.
  * What you will get is a lighter yet fully operational mesh data structure, just
  * without the burden of OpenGL code necessary for rendering!
@@ -15,13 +14,14 @@
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+using namespace cinolib;
+using namespace std;
+
 int main(int argc, char **argv)
 {
-    using namespace cinolib;
-
     QApplication a(argc, argv);
 
-    std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/sphere.mesh";
+    string s = (argc==2) ? string(argv[1]) : string(DATA_PATH) + "/sphere.mesh";
 
     DrawableTetmesh<> m(s.c_str());
 
