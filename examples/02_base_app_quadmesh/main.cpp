93fab9549ca1ed8cf6aee820ea995a8877d9f06c
P.E.Mazniker <60963077+goodengineer@users.noreply.github.com>
2020-09-09 16:17:10 +0300

    Update main.cpp

--- examples/02_base_app_quadmesh/main.cpp
+++ examples/02_base_app_quadmesh/main.cpp
@@ -2,10 +2,6 @@
  * It will show a GL canvas (and associated control panel) to interact
  * with a quadrilateral mesh.
  *
- * In case you don't need a GUI, you can drop the "Drawable" prefix from the mesh data type.
- * What you will get is a lighter yet fully operational mesh data structure, just
- * without the burden of OpenGL code necessary for rendering!
- *
  * Enjoy!
 */
 
@@ -15,6 +11,8 @@
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+#define DRAW_MESH TRUE
+
 int main(int argc, char **argv)
 {
     using namespace cinolib;
@@ -23,6 +21,7 @@ int main(int argc, char **argv)
 
     std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/cubespikes.obj";
 
+    #ifdef DRAW_MESH
     DrawableQuadmesh<> m(s.c_str());
 
     GLcanvas gui;
@@ -33,5 +32,14 @@ int main(int argc, char **argv)
     SurfaceMeshControlPanel<DrawableQuadmesh<>> panel(&m, &gui);
     QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_1), &gui), &QShortcut::activated, [&](){panel.show();});
 
+    #else
+    Quadmesh<> m(s.c_str());
+    // In case you don't need a GUI, you can drop the "Drawable" prefix from the mesh data type.
+    // What you will get is a lighter yet fully operational mesh data structure, just
+    // without the burden of OpenGL code necessary for rendering!
+    //Your model(mesh) specific processing code goes here:
+    
+    #endif
+    
     return a.exec();
 }
