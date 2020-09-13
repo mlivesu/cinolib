212bf181215808fa5a35cf9a75dbdc150237b2ee
P.E.Mazniker <60963077+goodengineer@users.noreply.github.com>
2020-09-13 08:52:49 +0300

    Update main.cpp

--- examples/15_polygon_measures/main.cpp
+++ examples/15_polygon_measures/main.cpp
@@ -18,7 +18,9 @@
 #include <cinolib/geometry/n_sided_poygon.h>
 
 using namespace cinolib;
-
+using namespace std;
+#define POLYGON_DIM 50
+#define WIREFRAME_WIDTH 3
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 int main(int argc, char **argv)
@@ -35,23 +37,23 @@ int main(int argc, char **argv)
     m.show_wireframe(true);
     m.show_marked_edge(false);
 
-    std::vector<vec3d> kernel;
+    vector<vec3d> kernel;
     polygon_kernel(m.vector_verts(), kernel);
     DrawablePolygonmesh<> k(kernel);
     k.poly_set_color(Color::PASTEL_RED());
 
     vec3d  sec_c, mic_c;
-    double sec_r, mic_r;
+    float sec_r, mic_r;
     smallest_enclosing_disk(m.vector_verts(), sec_c, sec_r);
     polygon_maximum_inscribed_circle(m.vector_verts(), mic_c, mic_r);
-    DrawablePolygonmesh<> sec(n_sided_polygon(sec_c, 50, sec_r, Z));
-    DrawablePolygonmesh<> mic(n_sided_polygon(mic_c, 50, mic_r, Z));
+    DrawablePolygonmesh<> sec(n_sided_polygon(sec_c, POLYGON_DIM, sec_r, Z));
+    DrawablePolygonmesh<> mic(n_sided_polygon(mic_c, POLYGON_DIM, mic_r, Z));
     sec.show_mesh_points();
     mic.show_mesh_points();
     sec.show_wireframe(true);
     mic.show_wireframe(true);
-    sec.show_wireframe_width(3);
-    mic.show_wireframe_width(3);
+    sec.show_wireframe_width(WIREFRAME_WIDTH);
+    mic.show_wireframe_width(WIREFRAME_WIDTH);
     sec.show_wireframe_color(Color::RED());
     mic.show_wireframe_color(Color::BLUE());
     sec.show_marked_edge(false);
@@ -76,14 +78,14 @@ int main(int argc, char **argv)
     QSlider::connect(&slider, &QSlider::valueChanged, [&]()
     {
         float t = static_cast<float>(slider.value()-slider.minimum())/static_cast<float>(slider.maximum()-slider.minimum());
-        m.vert(2) = vec3d(2,  3,0)*t + vec3d(2,1,0)*(1.0-t);
+        m.vert(2) = vec3d(2,3,0)*t + vec3d(2,1,0)*(1.0-t);
         m.vert(3) = vec3d(1,0.1,0)*t + vec3d(1,1,0)*(1.0-t);
-        m.vert(4) = vec3d(0,  3,0)*t + vec3d(0,1,0)*(1.0-t);
+        m.vert(4) = vec3d(0,3,0)*t + vec3d(0,1,0)*(1.0-t);
         m.update_p_tessellations();
         m.updateGL();
 
         profiler.push("Compute polygon kernel");
-        double area = polygon_kernel(m.vector_verts(), kernel);
+        float area = polygon_kernel(m.vector_verts(), kernel);
         profiler.pop();
         k = DrawablePolygonmesh<>(kernel);
         k.poly_set_color(Color::PASTEL_RED());
@@ -95,14 +97,14 @@ int main(int argc, char **argv)
         profiler.push("Compute maximum inscribed circle");
         polygon_maximum_inscribed_circle(m.vector_verts(), mic_c, mic_r);
         profiler.pop();
-        sec.vector_verts() = n_sided_polygon(sec_c, 50, sec_r, Z);
-        mic.vector_verts() = n_sided_polygon(mic_c, 50, mic_r, Z);
+        sec.vector_verts() = n_sided_polygon(sec_c, POLYGON_DIM, sec_r, Z);
+        mic.vector_verts() = n_sided_polygon(mic_c, POLYGON_DIM, mic_r, Z);
         sec.updateGL();
         mic.updateGL();
 
-        std::cout << "kernel area: " << area << std::endl;
-        std::cout << "radius of smallest enclosing circle: " << sec_r << std::endl;
-        std::cout << "radius of maximum inscribed circle: " << mic_r << "\n" << std::endl;
+        cout << "kernel area: " << area << endl;
+        cout << "radius of smallest enclosing circle: " << sec_r << endl;
+        cout << "radius of maximum inscribed circle: " << mic_r << "\n" << endl;
 
         canvas.updateGL();
     });
