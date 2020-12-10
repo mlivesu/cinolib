bd447354584d03504d3143709be6fb5bf4542cef
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2020-12-10 22:32:54 +0100

    numerically more stable 2x2 eigenvalues

--- include/cinolib/matrix.cpp
+++ include/cinolib/matrix.cpp
@@ -75,8 +75,7 @@ void eigen_decomposition_2x2(const double   a00,
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-// http://www.math.harvard.edu/archive/21b_fall_04/exhibits/2dmatrices/index.html
-//
+// https://lucidar.me/en/mathematics/singular-value-decomposition-of-a-2x2-matrix/
 CINO_INLINE
 void eigenvalues_2x2(const double   a00,
                      const double   a01,
@@ -85,11 +84,18 @@ void eigenvalues_2x2(const double   a00,
                            double & min,
                            double & max)
 {
-    double T = a00 + a11; // trace
-    double D = determinant_2x2(a00,a01,a10,a11);
+    double s1 = a00*a00 + a01*a01 + a10*a10 + a11*a11;
+    double s2 = sqrt(pow(a00*a00 + a01*a01 - a10*a10 - a11*a11,2) + 4*pow(a00*a10 + a01*a11,2));
 
-    min = T/2.0 - sqrt(T*T/4.0-D);
-    max = T/2.0 + sqrt(T*T/4.0-D);
+    max = sqrt((s1+s2)*0.5);
+    min = sqrt((s1-s2)*0.5);
+
+//    OLD (NUMERICALLY LESS STABLE) VERSION WAS BASED ON:
+//    http://www.math.harvard.edu/archive/21b_fall_04/exhibits/2dmatrices/index.html
+//    double T = a00 + a11; // trace
+//    double D = determinant_2x2(a00,a01,a10,a11);
+//    min = T/2.0 - sqrt(T*T/4.0-D);
+//    max = T/2.0 + sqrt(T*T/4.0-D);
 }
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
