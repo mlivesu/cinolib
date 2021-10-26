384e6a8fd45fa3a2a5e52cd22ade154f6f0b8d10
Marco Livesu <marco.livesu@gmail.com>
2021-10-26 18:09:30 +0200

    Update vec_mat_utils.cpp

--- include/cinolib/geometry/vec_mat_utils.cpp
+++ include/cinolib/geometry/vec_mat_utils.cpp
@@ -708,7 +708,12 @@ T mat_det(const T m[][d])
                                     m[1][0], m[1][1], m[1][2],
                                     m[2][0], m[2][1], m[2][2]);
 
-        default: assert(false && "mat_determinant: unsupported matrix size");
+        default:
+        {
+            typedef Eigen::Matrix<T,d,d,Eigen::RowMajor> M;
+            Eigen::Map<const M> tmp(m[0]);
+            return tmp.determinant();
+        }
     }
 }
 
@@ -752,7 +757,7 @@ void mat_transpose(const T m[][c], T tr[][r])
 
 template<uint d, typename T>
 CINO_INLINE
-bool mat_inverse(const T m[][d], T in[][d])
+void mat_inverse(const T m[][d], T in[][d])
 {
     switch(d)
     {
@@ -765,10 +770,15 @@ bool mat_inverse(const T m[][d], T in[][d])
             in[0][1] = -m[0][1] * one_over_det;
             in[1][0] = -m[1][0] * one_over_det;
             in[1][1] =  m[0][0] * one_over_det;
-            return one_over_det!=0; // false if the matrix is singular
         }
 
-        default: assert(false && "mat_inverse: unsupported matrix size");
+        default: // up to 4x4 matrices with Eigen
+        {
+            typedef Eigen::Matrix<T,d,d,Eigen::RowMajor> M;
+            Eigen::Map<const M> tmp_m(m[0]);
+            Eigen::Map<M> tmp_in(in[0]);
+            tmp_in = tmp_m.inverse();
+        }
     }
 }
 
