87cdcf0a8ba6eb626a3537588662b91efa32746b
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2022-01-09 23:24:30 +0100

    porting of example on iso contours

--- examples/17_iso_contours/main.cpp
+++ examples/17_iso_contours/main.cpp
@@ -1,42 +1,14 @@
-/* This sample program computes on-the-fly iso-contours
- * of a scalar field embedded in the vertices of a surface
- * mesh.
- *
- * Enjoy!
-*/
-
-#include <QApplication>
-#include <QSlider>
-#include <QPushButton>
-#include <QGridLayout>
 #include <cinolib/meshes/meshes.h>
 #include <cinolib/geodesics.h>
 #include <cinolib/profiler.h>
 #include <cinolib/drawable_isocontour.h>
-#include <cinolib/gl/qt/qt_gui_tools.h>
-
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#include <cinolib/gl/glcanvas.h>
 
 int main(int argc, char **argv)
 {
     using namespace cinolib;
 
-    QApplication a(argc, argv);
-
-    QWidget     window;
-    QSlider     sl_iso(Qt::Horizontal);
-    QPushButton but_tessellate("Tessellate");
-    QGridLayout layout;
-    GLcanvas    gui(&window);
-    sl_iso.setMaximum(100);
-    sl_iso.setMinimum(0);
-    sl_iso.setValue(50);
-    layout.addWidget(&sl_iso,0,0,1,9);
-    layout.addWidget(&but_tessellate,0,9,1,1);
-    layout.addWidget(&gui,2,0,1,10);
-    window.setLayout(&layout);
-    window.show();
-    window.resize(600,600);
+    GLcanvas gui;
 
     std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/circle.obj";
     DrawableTrimesh<> m(s.c_str());
@@ -44,34 +16,29 @@ int main(int argc, char **argv)
     m.show_texture1D(TEXTURE_1D_HSV_W_ISOLINES);
     m.show_wireframe(true);
     m.show_wireframe_transparency(0.5);
-    gui.push_obj(&m);
+    gui.push(&m);
 
-    DrawableIsocontour<> iso(m, 0.5);
+    float iso_val = 0.5;
+    DrawableIsocontour<> iso(m, iso_val);
     iso.thickness = 3.0;
-    gui.push_obj(&iso, false);
+    gui.push(&iso, false);
 
     Profiler profiler;
-
-    QSlider::connect(&sl_iso, &QSlider::valueChanged, [&]()
+    gui.callback_app_controls = [&]()
     {
-        profiler.push("update iso-contour");
-        iso = DrawableIsocontour<>(m, static_cast<float>(sl_iso.value())/100.0);
-        profiler.pop();
-        gui.updateGL();
-    });
+        if(ImGui::SliderFloat("iso", &iso_val, 0.0, 1.0))
+        {
+            iso = DrawableIsocontour<>(m,iso_val);
+            iso.thickness = 3.0;
+        }
+        if(ImGui::SmallButton("Tessellate"))
+        {
+            profiler.push("tessellate iso-contour");
+            iso.tessellate(m);
+            profiler.pop();
+            m.updateGL();
+        }
+    };
 
-    QSlider::connect(&but_tessellate, &QPushButton::clicked, [&]()
-    {
-        profiler.push("tessellate iso-contour");
-        iso.tessellate(m);
-        profiler.pop();
-        m.updateGL();
-        gui.updateGL();
-    });
-
-    // CMD+1 to show mesh controls.
-    SurfaceMeshControlPanel<DrawableTrimesh<>> panel(&m, &gui);
-    QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_1), &gui), &QShortcut::activated, [&](){panel.show();});
-
-    return a.exec();
+    return gui.launch();
 }
