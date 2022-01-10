ea5c7717e6cf96525bead54685ed5fe4c3bf8741
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2022-01-10 16:25:16 +0100

    porting of examples on coarse quad/hex layouts

--- examples/20_coarse_quad_layouts/main.cpp
+++ examples/20_coarse_quad_layouts/main.cpp
@@ -1,28 +1,15 @@
-/* This sample program computes the coarse quad
- * decomposition of a given quadrilateral mesh.
- *
- * Enjoy!
-*/
-
-#include <QApplication>
-#include <QSlider>
-#include <QPushButton>
-#include <QGridLayout>
 #include <cinolib/meshes/meshes.h>
 #include <cinolib/profiler.h>
-#include <cinolib/gl/qt/qt_gui_tools.h>
+#include <cinolib/gl/glcanvas.h>
 #include <cinolib/coarse_layout.h>
+#include <cinolib/gl/surface_mesh_controls.h>
 #include <cinolib/drawable_sphere.h>
-
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 int main(int argc, char **argv)
 {
     using namespace cinolib;
 
-    QApplication a(argc, argv);
-
-    std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "cubespikes.obj";
+    std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/cubespikes.obj";
     DrawableQuadmesh<> m(s.c_str());
 
     Profiler profiler;
@@ -31,21 +18,18 @@ int main(int argc, char **argv)
     profiler.pop();
     m.poly_color_wrt_label();
     m.show_marked_edge_color(Color::BLACK());
-    m.show_marked_edge_width(3);
-    m.edge_set_alpha(0.5);
+    m.show_marked_edge_width(5);
+    m.updateGL();
 
     GLcanvas gui;
-    gui.push_obj(&m);
-    gui.show();
+    SurfaceMeshControls<DrawableQuadmesh<>> controls(&m, &gui);
+    gui.push(&m);
+    gui.push(&controls);
 
     for(uint vid=0; vid<m.num_verts(); ++vid)
     {
-        if(m.vert_is_singular(vid)) gui.push_obj(new DrawableSphere(m.vert(vid), 0.5));
+        if(m.vert_is_singular(vid)) gui.push(new DrawableSphere(m.vert(vid), 20));
     }
 
-    // CMD+1 to show mesh controls.
-    SurfaceMeshControlPanel<DrawableQuadmesh<>> panel(&m, &gui);
-    QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_1), &gui), &QShortcut::activated, [&](){panel.show();});
-
-    return a.exec();
+    return gui.launch();
 }
--- examples/21_coarse_hex_layouts/main.cpp
+++ examples/21_coarse_hex_layouts/main.cpp
@@ -1,25 +1,12 @@
-/* This sample program computes the coarse block
- * decomposition of a given hexhedral mesh.
- *
- * Enjoy!
-*/
-
-#include <QApplication>
-#include <QSlider>
-#include <QPushButton>
-#include <QGridLayout>
 #include <cinolib/meshes/meshes.h>
 #include <cinolib/profiler.h>
-#include <cinolib/gl/qt/qt_gui_tools.h>
+#include <cinolib/gl/glcanvas.h>
+#include <cinolib/gl/volume_mesh_controls.h>
 #include <cinolib/coarse_layout.h>
-
-//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 int main(int argc, char **argv)
 {
     using namespace cinolib;
-
-    QApplication a(argc, argv);
 
     std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/ellipsoid.mesh";
     DrawableHexmesh<> m(s.c_str());
@@ -32,12 +19,9 @@ int main(int argc, char **argv)
     m.updateGL();
 
     GLcanvas gui;
-    gui.push_obj(&m);
-    gui.show();
+    VolumeMeshControls<DrawableHexmesh<>> menu(&m,&gui);
+    gui.push(&m);
+    gui.push(&menu);
 
-    // CMD+1 to show mesh controls.
-    VolumeMeshControlPanel<DrawableHexmesh<>> panel(&m, &gui);
-    QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_1), &gui), &QShortcut::activated, [&](){panel.show();});
-
-    return a.exec();
+    return gui.launch();
 }
