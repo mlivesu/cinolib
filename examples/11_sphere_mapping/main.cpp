42e69c5d0a9908e2587861ae491d8a0c2296f244
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2022-01-17 15:08:38 +0100

    simplified sphere mapping gui

--- examples/11_sphere_mapping/main.cpp
+++ examples/11_sphere_mapping/main.cpp
@@ -9,15 +9,13 @@ int main(int argc, char **argv)
     using namespace cinolib;
 
     std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/bunny.obj";
-    DrawableTrimesh<> m_xyz(s.c_str());
-    DrawableTrimesh<> m_uvw = m_xyz;
+    DrawableTrimesh<> m(s.c_str());
 
-    GLcanvas gui_xyz, gui_uvw;
-    gui_xyz.push(&m_xyz);
-    gui_uvw.push(&m_uvw);
+    GLcanvas gui;
+    gui.push(&m);
 
     Profiler profiler;
-    gui_xyz.callback_app_controls = [&]()
+    gui.callback_app_controls = [&]()
     {
         static int   n_iter        = 10;
         static float time_step     = 0.005;
@@ -26,25 +24,20 @@ int main(int argc, char **argv)
         if(ImGui::SmallButton("Map to Sphere"))
         {
             profiler.push("Compute MCF");
-            MCF(m_uvw, (uint)n_iter, (double)time_step);
+            MCF(m, (uint)n_iter, (double)time_step);
             profiler.pop();
-            m_uvw.updateGL();
-            gui_uvw.refit_scene();
+            m.updateGL();
+            gui.refit_scene();
         }
         if(ImGui::SmallButton("Reset"))
         {
-            m_uvw = m_xyz;
-            m_uvw.updateGL();
-            gui_uvw.refit_scene();
+            m = DrawableTrimesh<>(s.c_str());
+            gui.refit_scene();
         }
     };
 
-    // only the main window can host visual controls, so all menus are pushed in gui_xyz
-    SurfaceMeshControls<DrawableTrimesh<>> menu_xyz(&m_xyz, &gui_xyz, "object space");
-    SurfaceMeshControls<DrawableTrimesh<>> menu_uvw(&m_uvw, &gui_xyz, "sphere map");
-    gui_xyz.push(&menu_xyz);
-    gui_xyz.push(&menu_uvw);
+    SurfaceMeshControls<DrawableTrimesh<>> menu(&m, &gui);
+    gui.push(&menu);
 
-    // the main window launches the event loop for itself and for all other windows in the app
-    return gui_xyz.launch({&gui_uvw});
+    return gui.launch();
 }
