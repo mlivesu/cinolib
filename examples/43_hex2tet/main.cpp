b835c0a67e944d01976456e1341456637b89a321
Marco Livesu <marco.livesu@ge.imati.cnr.it>
2022-07-01 15:28:47 +0200

    Update main.cpp

--- examples/43_hex2tet/main.cpp
+++ examples/43_hex2tet/main.cpp
@@ -9,10 +9,10 @@ using namespace cinolib;
 int main(int argc, char *argv[])
 {
     std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/ellipsoid.mesh";
-    cinolib::Hexmesh<> hm(s.c_str());
-    cinolib::Tetmesh<> tm;
-    cinolib::hex_to_tets(hm,tm);
-    std::string basename = cinolib::get_file_path(s,true);
+    Hexmesh<> hm(s.c_str());
+    Tetmesh<> tm;
+    hex_to_tets(hm,tm);
+    std::string basename = get_file_path(s,true);
     tm.save((basename+"_tet.mesh").c_str());
     return 0;
 }
