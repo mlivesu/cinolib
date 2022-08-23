a7bb7b35cccd9b30e0cf6159c9c4bc6edd882689
Marco Livesu <marco.livesu@gmail.com>
2022-08-23 12:30:03 +0200

    bugfix

--- include/cinolib/drawable_segment_soup.h
+++ include/cinolib/drawable_segment_soup.h
@@ -91,7 +91,7 @@ class DrawableSegmentSoup: public std::vector<vec3d>, public DrawableObject
         bool  use_gl_lines        = false;        // cheaper rendering using GL_LINES instead of meshed cylinders
         bool  draw_joint_spheres  = false;        // fills the gap between adjacent segments with a small sphere
         bool  joint_sphere_subd   = 1;            // number of subdivisions for joint spheres
-        bool  segment_n_sides     = 16;           // number of sides for the segment cross-section
+        uint  segment_n_sides     = 16;           // number of sides for the segment cross-section
 };
 
 }
