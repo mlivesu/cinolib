5b0a939e655a49635c9f533c4ab6bb94e908f5ce
Marco Livesu <marco.livesu@gmail.com>
2022-09-16 22:15:07 +0200

    tiny refactor

--- include/cinolib/voxel_grid_to_hexmesh.cpp
+++ include/cinolib/voxel_grid_to_hexmesh.cpp
@@ -47,32 +47,32 @@ void voxel_grid_to_hexmesh(const VoxelGrid          & g,
     uint n_voxel =  g.dim[0]   * g.dim[1]   * g.dim[2];
     uint n_verts = (g.dim[0]+1)*(g.dim[1]+1)*(g.dim[2]+1);
     std::vector<int> vert_map(n_verts,-1); // to keep track of already existing vertices
-    for(uint voxel_id=0; voxel_id<n_voxel; ++voxel_id)
+    for(uint id=0; id<n_voxel; ++id)
     {
-        if(g.voxels[voxel_id]==VOXEL_BOUNDARY)
+        if(g.voxels[id]==VOXEL_BOUNDARY)
         {
-            vec3i vox_ijk = deserialize_3D_index(voxel_id,g.dim[1],g.dim[2]);
+            vec3i ijk = deserialize_3D_index(id,g.dim[1],g.dim[2]);
 
-            std::vector<uint> voxel_verts(8);
-            std::vector<uint> voxel_faces(6);
-            std::vector<bool> voxel_winding(6,false);
+            std::vector<uint> verts(8);
+            std::vector<uint> faces(6);
+            std::vector<bool> winding(6,false);
 
             // make verts
             for(uint i=0; i<8; ++i)
             {
-                uint v_ijk = serialize_3D_index(vox_ijk[0] + REFERENCE_HEX_VERTS[i][0],
-                                                vox_ijk[1] + REFERENCE_HEX_VERTS[i][1],
-                                                vox_ijk[2] + REFERENCE_HEX_VERTS[i][2],
-                                                g.dim[1]+1,g.dim[2]+1);
-                if(vert_map[v_ijk]<0)
+                uint vid = serialize_3D_index(ijk[0] + REFERENCE_HEX_VERTS[i][0],
+                                              ijk[1] + REFERENCE_HEX_VERTS[i][1],
+                                              ijk[2] + REFERENCE_HEX_VERTS[i][2],
+                                              g.dim[1]+1,g.dim[2]+1);
+                if(vert_map[vid]<0)
                 {
                     vec3d p;
-                    p[0] = g.bbox.min[0] + g.len*vox_ijk[0] + g.len*REFERENCE_HEX_VERTS[i][0];
-                    p[1] = g.bbox.min[1] + g.len*vox_ijk[1] + g.len*REFERENCE_HEX_VERTS[i][1];
-                    p[2] = g.bbox.min[2] + g.len*vox_ijk[2] + g.len*REFERENCE_HEX_VERTS[i][2];
-                    vert_map[v_ijk] = m.vert_add(p);
+                    p[0] = g.bbox.min[0] + g.len*ijk[0] + g.len*REFERENCE_HEX_VERTS[i][0];
+                    p[1] = g.bbox.min[1] + g.len*ijk[1] + g.len*REFERENCE_HEX_VERTS[i][1];
+                    p[2] = g.bbox.min[2] + g.len*ijk[2] + g.len*REFERENCE_HEX_VERTS[i][2];
+                    vert_map[vid] = m.vert_add(p);
                 }
-                voxel_verts[i] = vert_map[v_ijk];
+                verts[i] = vert_map[vid];
             }
 
             // make faces
@@ -80,21 +80,21 @@ void voxel_grid_to_hexmesh(const VoxelGrid          & g,
             {
                 std::vector<uint> face =
                 {
-                    voxel_verts[HEXA_FACES[ii][0]],
-                    voxel_verts[HEXA_FACES[ii][1]],
-                    voxel_verts[HEXA_FACES[ii][2]],
-                    voxel_verts[HEXA_FACES[ii][3]]
+                    verts[HEXA_FACES[ii][0]],
+                    verts[HEXA_FACES[ii][1]],
+                    verts[HEXA_FACES[ii][2]],
+                    verts[HEXA_FACES[ii][3]]
                 };
                 int fid = m.face_id(face);
                 if(fid<0)
                 {
                     fid = m.face_add(face);
-                    voxel_winding[ii] = true;
+                    winding[ii] = true;
                 }
-                voxel_faces[ii] = fid;
+                faces[ii] = fid;
             }
             // add voxel
-            m.poly_add(voxel_faces,voxel_winding);
+            m.poly_add(faces,winding);
         }
     }
 }
