c1b44dbb2867cbd1080ff54bdd5ed6f07b3f4d8d
Marco Livesu <marco.livesu@gmail.com>
2022-11-13 15:09:48 +0100

    suppress MSVC warning

--- include/cinolib/min_max_inf.h
+++ include/cinolib/min_max_inf.h
@@ -44,15 +44,15 @@ namespace cinolib
     // https://stackoverflow.com/questions/20016600/negative-infinity
     static_assert(std::numeric_limits<double>::is_iec559, "IEEE 754 required");
 
-    const double min_double = std::numeric_limits<double>::min();
-    const float  min_float  = std::numeric_limits<float>::min();
-    const int    min_int    = std::numeric_limits<int>::min();
-    const uint   min_uint   = std::numeric_limits<uint>::min();
+    const double       min_double = std::numeric_limits<double>::min();
+    const float        min_float  = std::numeric_limits<float>::min();
+    const int          min_int    = std::numeric_limits<int>::min();
+    const unsigned int min_uint   = std::numeric_limits<unsigned int>::min();
 
-    const double max_double = std::numeric_limits<double>::max();
-    const float  max_float  = std::numeric_limits<float>::max();
-    const int    max_int    = std::numeric_limits<int>::max();
-    const uint   max_uint   = std::numeric_limits<uint>::max();
+    const double       max_double = std::numeric_limits<double>::max();
+    const float        max_float  = std::numeric_limits<float>::max();
+    const int          max_int    = std::numeric_limits<int>::max();
+    const unsigned int max_uint   = std::numeric_limits<unsigned int>::max();
 
     const double inf_double = std::numeric_limits<double>::infinity();
     const float  inf_float  = std::numeric_limits<float>::infinity();
