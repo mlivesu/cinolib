54377d535ae00de247f5a5dadd8722b02d9ee7b8
Marco Livesu <marco.livesu@gmail.com>
2023-07-18 16:33:22 +0200

    fix STB image write

--- external/stb/stb_image_write.h
+++ external/stb/stb_image_write.h
@@ -1,3 +1,4 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 /* stb_image_write - v1.16 - public domain - http://nothings.org/stb
    writes out PNG/BMP/TGA/JPEG/HDR images to C stdio - Sean Barrett 2010-2015
                                      no warranty implied; use at your own risk
