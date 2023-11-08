a494e14c7c6dfcc93deec8d881ecc4b144e42023
Marco Livesu <marco.livesu@gmail.com>
2023-11-08 16:23:32 +0100

    pop_push facility for profiler

--- include/cinolib/profiler.h
+++ include/cinolib/profiler.h
@@ -64,6 +64,7 @@ class Profiler
 
         void   push(const std::string & key);
         double pop (const bool print_time = true, const std::string extra_string = "");
+        double pop_push(const std::string & new_key, const bool print_time = true, const std::string extra_string = "");
 
         //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
