9c50a576e3e84a6f7b4a1ec633397d7a4646d48b
Marco Livesu <marco.livesu@gmail.com>
2024-03-12 09:54:54 +0100

    added 1-4 triangle midpoint subdivision

---
+++ include/cinolib/subdivision_1_to_4.h
@@ -0,0 +1,56 @@
+/********************************************************************************
+*  This file is part of CinoLib                                                 *
+*  Copyright(C) 2024: Marco Livesu                                              *
+*                                                                               *
+*  The MIT License                                                              *
+*                                                                               *
+*  Permission is hereby granted, free of charge, to any person obtaining a      *
+*  copy of this software and associated documentation files (the "Software"),   *
+*  to deal in the Software without restriction, including without limitation    *
+*  the rights to use, copy, modify, merge, publish, distribute, sublicense,     *
+*  and/or sell copies of the Software, and to permit persons to whom the        *
+*  Software is furnished to do so, subject to the following conditions:         *
+*                                                                               *
+*  The above copyright notice and this permission notice shall be included in   *
+*  all copies or substantial portions of the Software.                          *
+*                                                                               *
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
+*  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE *
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      *
+*  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS *
+*  IN THE SOFTWARE.                                                             *
+*                                                                               *
+*  Author(s):                                                                   *
+*                                                                               *
+*     Marco Livesu (marco.livesu@gmail.com)                                     *
+*     http://pers.ge.imati.cnr.it/livesu/                                       *
+*                                                                               *
+*     Italian National Research Council (CNR)                                   *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)    *
+*     Via de Marini, 6                                                          *
+*     16149 Genoa,                                                              *
+*     Italy                                                                     *
+*********************************************************************************/
+#ifndef CINO_SUBDIVISION_1_TO_4_H
+#define CINO_SUBDIVISION_1_TO_4_H
+
+#include <cinolib/meshes/trimesh.h>
+
+namespace cinolib
+{
+
+// Splits each mesh triangle into 4 subtriangles, splitting mesh edges at their midpoint
+//
+template<class M, class V, class E, class P>
+CINO_INLINE
+void subdivision_1_to_4(Trimesh<M,V,E,P> & m);
+
+}
+
+#ifndef  CINO_STATIC_LIB
+#include "subdivision_1_to_4.cpp"
+#endif
+
+#endif // CINO_SUBDIVISION_1_TO_4_H
